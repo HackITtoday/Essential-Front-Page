4f6c69a9fcf0e1da857e88ff0aa3525b191f3810
marcus hitchins <marcus7777@gmail.com>
2014-09-05 14:23:06 +0100

    more Grunt not working

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,17 +7,48 @@ module.exports = function(grunt) {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+    },
+    build: {
+      src: 'src/<%= pkg.name %>.js',
+      dest: 'build/<%= pkg.name %>.min.js'
+    },
+    watch: {
+      express: {
+        files:  [ '*.htm' ],
+        tasks:  [ 'express:dev' ],
+        options: {
+          spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
+        }
+      }
+    },
+    express: {
+      options: {
+      // Override defaults here
+      },
+      dev: {
+        options: {
+          script: '/usr/local/lib/node_modules/bower/node_modules/request/node_modules/hawk/lib/server.js'
+        }
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('server', [ 'express:dev', 'watch' ])
+  // load plugins
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-express-server');
 
+  // Define tasks
+  grunt.registerTask('build', ['coffee', 'stylus', 'copy']);
+  grunt.registerTask('start', ['build', 'express:dev', 'watch']);
+  grunt.registerTask('default', 'start');
 };
